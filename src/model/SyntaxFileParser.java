2f0fa92e5647a09c50943a2435c55833d574a6a6
Ying Wang <yingw787@gmail.com>
2015-10-18 01:58:39 -0400

    Made some edits to Syntax.properties to get SyntaxFileParser.java up and running; SyntaxFileParser.java cannot parse bracket regexes, not sure if can, but everything else can detect. Commented code provides guidance.

--- src/model/SyntaxFileParser.java
+++ src/model/SyntaxFileParser.java
@@ -1,17 +1,55 @@
 package model;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class SyntaxFileParser {
 
 	private static final String SYNTAX_FILE_LOCATION = "resources/languages/Syntax.properties";
-
+	private HashMap<Pattern, String> map = new HashMap<Pattern, String>();
+	
+	// TODO: Still need to integrate regexes for ListStart, ListEnd, GroupStart, GroupEnd
+	
 	public SyntaxFileParser(){
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File syntaxFile = new File(classLoader.getResource(SYNTAX_FILE_LOCATION).getFile());
+		
+		try(Scanner scanner = new Scanner(syntaxFile))
+		{
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				if(line.charAt(0) != '#'){
+					String newLine = line.replace(" = ", " ");
+					String[] commands = newLine.split(" ");
+					
+					Pattern pattern = Pattern.compile(commands[1]);
+//					Matcher matcher = pattern.matcher("# forward");
+//					boolean b = matcher.matches();
+					map.put(pattern, commands[0]);	
+				
+				}
+			}
+		}
+		catch(IOException e){
+			
+		}
 		
 	}
 	
 	
-	public static void main(String[] args){
-		System.out.println("Hello World!");
+	public HashMap<Pattern, String> retrieveMap(){
+		return map; 
 	}
+	
+//	public static void main(String[] args){
+//
+//		SyntaxFileParser parser = new SyntaxFileParser();
+//	
+//	}
 	
 	
 }
