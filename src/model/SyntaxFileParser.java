609714b0b781090b105f08869c990a6f6b6ffa74
Ying Wang <yingw787@gmail.com>
2015-10-18 01:27:08 -0400

    Created two separate file parsers since language and syntax files are dramatically different

---
+++ src/model/SyntaxFileParser.java
@@ -0,0 +1,17 @@
+package model;
+
+public class SyntaxFileParser {
+
+	private static final String SYNTAX_FILE_LOCATION = "resources/languages/Syntax.properties";
+
+	public SyntaxFileParser(){
+		
+	}
+	
+	
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+	
+	
+}
